586d34d9efa252f4de22d9f2d8f0643e2bc17d60
Siddharth <siddhu2588@gmail.com>
2021-10-19 15:26:09 +0530

    Myntra Clone

---
+++ index.js
@@ -0,0 +1,12 @@
+function clickOnTee(){
+    location.href="tshirt.html"
+    
+}
+
+function clickOnLogo(){
+    location.href="index.html"
+}
+
+function clickButton(){
+    location.href="cart.html"
+}
